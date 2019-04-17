6a7ab7e326f69fd075eab8bc241256723374f829
sudeyu <1035528554@qq.com>
2019-04-17 17:37:59 +0800

    websocket

---
+++ src/main/java/com/study/information_push/config/WebSocketConfig.java
@@ -0,0 +1,25 @@
+package com.study.information_push.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+/**
+ * @author sdy
+ * @date 2019/4/17 14:19
+ */
+@Component
+public class WebSocketConfig {
+
+    /**
+     * ServerEndpointExporter 作用
+     *
+     * 这个Bean会自动注册使用@ServerEndpoint注解声明的websocket endpoint
+     *
+     * @return
+     */
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
+}
